cdbf5d2568f89f3e2d7fb556afcc28a8185345f3
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2015-12-23 22:58:21 +0900

    全体の大枠部分を作成

---
+++ Sdm2Edm/Converter.fs
@@ -0,0 +1,15 @@
+﻿module Sdm2Edm.Converter
+
+open Sdm
+open Edm
+
+let convertComponent (r: Realizer) (c: Component) : Cell list =
+  []
+
+let convertPage (r: Realizer) (page: Page) : Sheet =
+  let sheetName = page.Name
+  { Sheet.Name = sheetName
+    Cells = page.Components |> List.collect (convertComponent r) }
+
+let convert (r: Realizer) (pages: Page list) : Sheet list =
+  pages |> List.map (convertPage r)
---
+++ Sdm2Edm/Realizer.fs
@@ -0,0 +1,18 @@
+﻿namespace Sdm2Edm
+
+open System.Collections.Generic
+
+[<AbstractClass>]
+type Realizer () =
+  let usedNamesCountTable = Dictionary<string, int>()
+
+  abstract SheetName: string -> string
+
+  default __.SheetName(name) =
+    match usedNamesCountTable.TryGetValue(name) with
+    | true, used ->
+        usedNamesCountTable.[name] <- used + 1
+        sprintf "%s(%d)" name (used + 1)
+    | false, _ ->
+        usedNamesCountTable.Add(name, 1)
+        name
