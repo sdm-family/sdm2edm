a73a1c24b6b48c392842c1240ff69cba5a079b3c
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2016-04-08 20:31:59 +0900

    項目ごとに出すスライドを作成

--- presentations/NLNagoya/Program.fs
+++ presentations/NLNagoya/Program.fs
@@ -8,25 +8,38 @@ let width = 79
 let height = 36
 
 let pages =
-  [ TitlePage { MainTitle = "No more Legacy documents"
-                SubTitle = "Excel方眼紙でプレゼン"
-                Speaker = "bleis-tift"
-                Date = DateTime(2016, 4, 16) }
-    ImageAndListPage { Heading = "自己紹介"
-                       ImagePath = "at.png"
-                       Items = [ TextListItem "id:bleis-tift / @bleis"
-                                 TextListItem "株式会社オンザロード"
-                                 TextListItem "F#が好き"
-                                 TextListItem "Excel方眼紙は嫌い" ] }
-    ListPage { Heading = "今日話すこと"
-               Items = [ TextListItem "関数型プログラミング言語を使おう！"
-                         Nested ("関数型プログラミング言語による身近な問題の解決",
-                                 [ TextListItem "あいつを倒す・・・倒したい"
-                                   TextListItem "ざっくり仕組みの説明" ])
-                         TextListItem "応用例の紹介" ] }
-    SectionPage { SectionName = "関数型プログラミング言語を使おう！" }
-    SectionPage { SectionName = "関数プログラミング言語による身近な問題の解決" }
-    SectionPage { SectionName = "応用例の紹介" } ]
+  [ yield TitlePage { MainTitle = "No more Legacy documents"
+                      SubTitle = "Excel方眼紙でプレゼン"
+                      Speaker = "bleis-tift"
+                      Date = DateTime(2016, 4, 16) }
+    yield ImageAndListPage { Heading = "自己紹介"
+                             ImagePath = "at.png"
+                             Items = [ TextListItem "id:bleis-tift / @bleis"
+                                       TextListItem "株式会社オンザロード"
+                                       TextListItem "F#が好き"
+                                       TextListItem "Excel方眼紙は嫌い" ] }
+    yield ListPage { Heading = "今日話すこと"
+                     Items = [ TextListItem "関数型プログラミング言語を使おう！"
+                               Nested ("関数型プログラミング言語による身近な問題の解決",
+                                       [ TextListItem "あいつを倒す・・・倒したい"
+                                         TextListItem "ざっくり仕組みの説明" ])
+                               TextListItem "応用例の紹介"
+                               TextListItem "まとめ" ] }
+
+    yield SectionPage { SectionName = "関数型プログラミング言語を使おう！" }
+
+    yield SectionPage { SectionName = "関数プログラミング言語による身近な問題の解決" }
+    for i in 1..3 do
+      yield ListPage { Heading = "身近な問題の例"
+                       Items = List.init i (fun _ -> TextListItem "Excel方眼紙") }
+    yield ListPage { Heading = "ニッポンの素敵なExcel方眼紙"
+                     Items = [ TextListItem "todo" ] }
+
+    yield SectionPage { SectionName = "応用例の紹介" }
+
+    yield SectionPage { SectionName = "まとめ" }
+    yield ListPage { Heading = "まとめ"
+                     Items = [ TextListItem "TODO" ] } ]
 
 [<EntryPoint>]
 let main argv =
