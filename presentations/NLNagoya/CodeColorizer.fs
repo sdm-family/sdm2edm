4de81ef6fb2e3221c07ca68a46a0ed32ce4dd559
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2016-04-15 14:38:06 +0900

    コードの色をよりわかりやすくした

--- presentations/NLNagoya/CodeColorizer.fs
+++ presentations/NLNagoya/CodeColorizer.fs
@@ -36,19 +36,50 @@ module CodeColorizer =
         let str = line.Substring(tok.LeftColumn, tok.RightColumn - tok.LeftColumn + 1)
         yield (str, tok.ColorClass) ]
 
+  type private TokKind =
+    | TypeKeyword
+    | DUBar
+    | TypeAnot
+    | Other
+
+  let mutable private prevTokKind = Other
+  let mutable private prevStr = ""
+
   let private toSegment (str, colorClass) =
+    if prevTokKind = TypeAnot then
+      match str with
+      | ";" -> prevTokKind <- Other
+      | "\n" when prevStr <> ":" -> prevTokKind <- Other
+      | _ -> ()
+    if not (String.IsNullOrWhiteSpace(str)) then
+      prevStr <- str
     let font color = Font.create (FontName "MeiryoKe_Console") (NoFontSize) NoFontStyle color
     { RichTextSegment.Value = str
       FontInfo =
         match colorClass with
-        | FSharpTokenColorKind.Default -> font RgbColor.black
-        | FSharpTokenColorKind.UpperIdentifier -> font RgbColor.black
+        | FSharpTokenColorKind.Default ->
+            if str = "|" then prevTokKind <- DUBar
+            font RgbColor.black
+        | FSharpTokenColorKind.UpperIdentifier ->
+            match prevTokKind with
+            | TypeKeyword -> prevTokKind <- Other; font (RgbColor.create (43, 145, 175))
+            | TypeAnot -> prevTokKind <- TypeAnot; font (RgbColor.create (43, 145, 175))
+            | DUBar -> prevTokKind <- Other; font (RgbColor.create (255, 128, 0))
+            | _ -> font RgbColor.black
+        | FSharpTokenColorKind.Identifier ->
+            match prevTokKind with
+            | TypeAnot -> font (RgbColor.create (43, 145, 175))
+            | _ -> font RgbColor.black
         | FSharpTokenColorKind.Number -> font RgbColor.black
         | FSharpTokenColorKind.String -> font (RgbColor.create (163, 21, 21))
-        | FSharpTokenColorKind.Keyword -> font (RgbColor.create (0, 0, 255))
+        | FSharpTokenColorKind.Keyword ->
+            if str = "type" then prevTokKind <- TypeKeyword
+            elif str = "of" then prevTokKind <- TypeAnot
+            font (RgbColor.create (0, 0, 255))
         | FSharpTokenColorKind.PreprocessorKeyword -> font (RgbColor.create (0, 0, 255))
-        | FSharpTokenColorKind.Operator -> font (RgbColor.create (255, 0, 0))
-        | FSharpTokenColorKind.Identifier -> font RgbColor.black
+        | FSharpTokenColorKind.Operator ->
+            if str = ":" then prevTokKind <- TypeAnot
+            font (RgbColor.create (255, 0, 0))
         | FSharpTokenColorKind.InactiveCode -> font (RgbColor.create (157, 157, 157))
         | FSharpTokenColorKind.Comment -> font (RgbColor.create (0, 128, 0)) }
 
